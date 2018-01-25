4f89cfe7f738eaa136366e5e9f69bb8cc65b2a1d
Raojayou <jose_10tonio@hotmail.com>
2018-01-25 19:41:30 +0100

    Correciones de algunos fallos

--- database/factories/MuseoFactory.php
+++ database/factories/MuseoFactory.php
@@ -7,13 +7,18 @@ $factory->define(App\Museo::class, function (Faker $faker) {
     $time1 = Carbon::createFromTimestamp($faker->dateTimeThisDecade()->getTimestamp());
     $time2 = Carbon::createFromTimestamp($faker->dateTimeThisDecade()->getTimestamp());
     return [
-        'horario_de_apertura' => $faker->randomDigit,
-        'pagina_web' => $faker->address,
-        'redes_sociales' => $faker->word,
-        'tipo_de_museo' => $faker->word,
-        'epoca_de_las_obras' => $faker->word,
-        'descripcion' => $faker->realText(255),
+        'name' =>$faker->domainName,
+        'horario'=> $faker->randomDigit,
+        'web' => $faker->address,
+        'social' => $faker->word,
+        'type'=> $faker->word,
+        'period' => $faker->word,
+        'description' => $faker->realText(255),
         'created_at' => ($time1 < $time2) ? $time1 : $time2,
         'updated_at' => ($time1 > $time2) ? $time1 : $time2
     ];
 });
+
+
+
+
